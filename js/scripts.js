4f5057ba35f370ab69fe314637ca5707256e256a
Stewart ilondanga <stilondanga@gmail.com>
2018-09-11 21:28:04 +0300

    add js/scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+/*var navigate = (function() {
+	$('.dd').toggle();
+	$('.dd_btn').click(function() {
+		var dataName = $(this).attr('data-name');
+		$('.dd').hide();
+		$('.' + dataName).toggle();
+	});
+})();
+
+const times = document.getElementById('times');
+const iconContainer = document.getElementById('info-container');
+// let infoP = document.getElementById('info');
+times.onclick = function() {
+	click();
+};
+function click() {
+	iconContainer.style.display = 'none';
+}
